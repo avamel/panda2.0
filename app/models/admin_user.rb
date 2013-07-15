b13ce119e52d0206cc6d8d1da0c3ec3c1eaba4fd
avamel <xnuamx@gmail.com>
2013-07-15 18:06:47 +0300

    fixing admin_user

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -4,4 +4,16 @@ class AdminUser < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, 
          :recoverable, :rememberable, :trackable, :validatable
+
+  after_destroy :ensure_an_admin_remains
+
+
+  private
+
+  def ensure_an_admin_remains
+    if AdminUser.count.zero?
+      raise "Последний пользователь не может быть удален!"
+    end
+  end
+
 end
