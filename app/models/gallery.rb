4ce5bb11b36ce8fe4d3ae730ccde2f5d2056b760
xamenrax <xamenrax@gmail.com>
2013-07-16 20:08:06 +0300

    fix slider show tour

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,7 +1,7 @@
 class Gallery < ActiveRecord::Base
   belongs_to :imageable, polymorphic: true
 
-  has_attached_file :attachment, :styles => { :slider_thumb => "104x52#", :thumb  => "70x60>", :medium => "200x200" }, :url  => "/assets/galleries/:id/:style/:basename.:extension"
+  has_attached_file :attachment, :styles => { :slider_thumb => "104x52#", :thumb  => "70x60>", :slider => "670x400" }, :url  => "/assets/galleries/:id/:style/:basename.:extension"
 
   validates :title, presence: true
 
