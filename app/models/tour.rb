d16f79c0863f96640384d1801bff532995c54a2e
avamel <xnuamx@gmail.com>
2013-07-16 12:57:07 +0300

    fist commit

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,6 +1,7 @@
 class Tour < ActiveRecord::Base
   has_many :galleries, :as => :imageable, dependent: :destroy
   has_many :days, dependent: :destroy
+  belongs_to :country, :through => :country_tours
 
   validates :title, presence: true
   validates :overview, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130715175336) do
+ActiveRecord::Schema.define(version: 20130716094934) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -67,6 +67,13 @@ ActiveRecord::Schema.define(version: 20130715175336) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "region"
+  end
+
+  create_table "country_tours", force: true do |t|
+    t.integer  "country_id"
+    t.integer  "tour_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "days", force: true do |t|
