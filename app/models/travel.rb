66e8e530212059b6db997bf1b4af48862b4fde93
avamel <xnuamx@gmail.com>
2013-07-18 13:33:22 +0300

    add travel add fixing validates

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -5,6 +5,11 @@ class Travel < ActiveRecord::Base
   has_attached_file :teaser, :styles => { :thumb => ["210x180#", :jpg] }
 
 
+  validates :title, presence: true
+  validates :preview, presence: true
+  validates :overview, presence: true
+
+
   def should_generate_new_friendly_id?
     new_record? || slug.blank?
   end
