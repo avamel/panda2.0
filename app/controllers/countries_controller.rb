3a8cfbaad8ac4f43cbe792f266bb34ad74999a99
xamenrax <xamenrax@gmail.com>
2013-07-18 14:53:50 +0300

    finish travels

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,8 +1,14 @@
 class CountriesController < ApplicationController
   def index
+    @tours = Tour.order("clicks DESC").includes(:currency).limit(11)
+    @travels = Travel.take(11)
+    @month_country = Month.where(:activate => true).order(:created_at).last
   end
 
   def show
+    # @newest_tours = Tour.last(3)
+    @newest_tours = Tour.take(3)
+    @month_country = Month.where(:activate => true).order(:created_at).last
     @country = Country.includes(tours: [:galleries, :tour_dates]).find(params[:id])
   end
 end
--- db/migrate/20130717151313_add_clicks_to_tour.rb
+++ db/migrate/20130717151313_add_clicks_to_tour.rb
@@ -1,5 +1,5 @@
 class AddClicksToTour < ActiveRecord::Migration
   def change
-    add_column :tours, :clicks, :integer
+    add_column :tours, :clicks, :integer, :default => 0
   end
 end
