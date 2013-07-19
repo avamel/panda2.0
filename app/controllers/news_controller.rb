af9eba7d8b82d720c0492e9dcee1d896ffa529ae
xamenrax <xamenrax@gmail.com>
2013-07-19 13:41:21 +0300

    add show pages to news and travels

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,10 +1,14 @@
 class NewsController < ApplicationController
 
   def index
-    news = News.all
+    @news = News.all
   end
 
   def show
-    @news = News.find params[:id]
+    @news_item = News.find params[:id]
+    @news = News.last(5)
+    @newest_tours = Tour.take(3)
+    @month_country = Country.where(month_country: true).first
+    # @country = Country.includes(tours: [:galleries, :tour_dates]).find params
   end
 end
